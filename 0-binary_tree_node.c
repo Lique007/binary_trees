bcf5229e1c4c863718a7af5c68b7cd55acd1c66b
Lique007 <angeliquehilario019@gmail.com>
2022-11-15 13:24:57 -0800

    new node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/* Create a node */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+int main(void)
+	
+struct *node createNode(value) {
+struct *binary_tree_node  newNode = malloc(sizeof(struct node));
+	newNode -> item = value;
+	newNode -> left = NULL;
+	newNode -> right = NULL;
+
+	return newNode;
+}
+
